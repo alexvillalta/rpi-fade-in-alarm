ebf233def2a06f8e3915e719416073201e2f9ceb
Alex Villalta <926316+alexvillalta@users.noreply.github.com>
2025-11-02 17:44:15 -0500

    moved light settings to lights.py, removed settings.py

--- lights.py
+++ lights.py
@@ -6,21 +6,46 @@ Usage:
 """
 
 from lifxlan import Light, Group
+from dotenv import load_dotenv
+import os
 import time
-import settings
+
+# Wake-up time parameters in seconds
+
+fade_in_stage_seconds = 600
+end_stage_seconds = 300 
+
+# Light color stages
+#   From https://github.com/mclarkk/lifxlan: 
+#       Each color is a HSBK list of values: [hue (0-65535), saturation (0-65535), brightness (0-65535), Kelvin (2500-9000)]
+
+start_stage_color = [0, 0, 0, 7000]
+fade_in_stage_color = [0, 0, 65535, 7000]
+end_stage_color = [0, 0, 32807, 2000]
+
+# Retrieve LIFX light mac and ip from a .env file in this script's directory
+# See https://github.com/mclarkk/lifxlan for details on getting these using lifxlan
+
+load_dotenv()
+light_1_mac = os.getenv("LIGHT_1_MAC")
+light_1_ip = os.getenv("LIGHT_1_IP")
+light_2_mac = os.getenv("LIGHT_2_MAC")
+light_2_ip = os.getenv("LIGHT_2_IP")
 
 # Set up LIFX objects
-light_1 = Light(settings.light_1_mac, settings.light_1_ip)
-light_2 = Light(settings.light_2_mac, settings.light_2_ip)
+
+light_1 = Light(light_1_mac, light_1_ip)
+light_2 = Light(light_2_mac, light_2_ip)
 g = Group([light_1, light_2])
 
 # Run wake-up sequence
-fade_in_stage_milliseconds = settings.fade_in_stage_seconds * 1000
+
+fade_in_stage_milliseconds = fade_in_stage_seconds * 1000
 g.set_power(0, 1)
-g.set_color(settings.start_stage_color, 1)
+g.set_color(start_stage_color, 1)
 g.set_power(1, 1)
-g.set_color(settings.fade_in_stage_color, fade_in_stage_milliseconds)
-time.sleep(settings.fade_in_stage_seconds)
-g.set_color(settings.end_stage_color, 1)
-time.sleep(settings.end_stage_seconds)
+g.set_color(fade_in_stage_color, fade_in_stage_milliseconds)
+time.sleep(fade_in_stage_seconds)
+g.set_color(end_stage_color, 1)
+time.sleep(end_stage_seconds)
 g.set_power(0, 1)
