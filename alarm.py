0b917ac105889eb55edb73ddaac28141a9570ab5
Alex Villalta <926316+alexvillalta@users.noreply.github.com>
2025-11-01 21:09:55 -0400

    corrections to imported settings variables

--- alarm.py
+++ alarm.py
@@ -3,19 +3,19 @@ import time
 import settings
 
 # Set up LIFX objects
-light_1 = Light(light_1_mac, light_1_ip)
-light_2 = Light(light_2_mac, light_2_ip)
+light_1 = Light(settings.light_1_mac, settings.light_1_ip)
+light_2 = Light(settings.light_2_mac, settings.light_2_ip)
 g = Group([light_1, light_2])
 
 # Minor settings processing
-fade_in_stage_milliseconds = fade_in_sec * 1000
+fade_in_stage_milliseconds = settings.fade_in_stage_seconds * 1000
 
 # Run wake-up sequence
 g.set_power(0, 1)
-g.set_color(start_stage_color, 1)
+g.set_color(settings.start_stage_color, 1)
 g.set_power(1, 1)
-g.set_color(fade_in_stage_color, fade_in_stage_milliseconds)
-time.sleep(fade_in_stage_seconds)
-g.set_color(end_stage_color, 1)
-time.sleep(end_stage_color)
+g.set_color(settings.fade_in_stage_color, fade_in_stage_milliseconds)
+time.sleep(settings.fade_in_stage_seconds)
+g.set_color(settings.end_stage_color, 1)
+time.sleep(settings.end_stage_seconds)
 g.set_power(0, 1)
